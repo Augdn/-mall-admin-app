82a2e91c8aa1f7b288dcc656532b5943ef3d6bc9
JiaLao <1146589518@qq.com>
2023-11-11 19:42:34 +0800

    分组件

--- src/store/index.js
+++ src/store/index.js
@@ -5,14 +5,23 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    // 用于切换菜单的闭合状态
+    collapsed: false,
+
   },
   getters: {
   },
   // 里面定义方法，操作state的方法
   mutations: {
+    changeCollapsed(state) {
+      state.collapsed = !state.collapsed;
+    },
   },
   // 操作异步操作mutations
   actions: {
+    changeCollapsed({ commit }) {
+      commit('changeCollapsed');
+    },
   },
   modules: {
   },
---
+++ src/views/layout/components/sildeNav.vue
@@ -0,0 +1,34 @@
+<template>
+
+      <div class="main-header">
+        <a-button type="primary" style="margin-bottom: 16px" @click="toggleCollapsed">
+          <a-icon :type="$store.state.collapsed ? 'menu-unfold' : 'menu-fold'" />
+        </a-button>
+        <div class="breadcrumb">
+          <a-breadcrumb>
+            <a-breadcrumb-item>首页</a-breadcrumb-item>
+            <a-breadcrumb-item><a href="">统计</a></a-breadcrumb-item>
+          </a-breadcrumb>
+        </div>
+        <ul class="use-info">
+            <li>asdasd
+              <a-icon type="down"></a-icon>
+            </li>
+            <li>退出</li>
+        </ul>
+      </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    toggleCollapsed() {
+      this.$store.dispatch('changeCollapsed');
+    },
+  },
+};
+</script>
+
+<style>
+
+</style>
