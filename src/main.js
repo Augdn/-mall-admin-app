8f7f2ec1690861c91bc6e2579d796a4c9a0f9c82
JiaLao <1146589518@qq.com>
2023-11-11 16:46:49 +0800

    系统初始布局

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
+import '@/assets/css/reset.less';
 import Vue from 'vue';
 import App from './App.vue';
+import './plugins/ant-design-vue';
 import router from './router';
 import store from './store';
 
